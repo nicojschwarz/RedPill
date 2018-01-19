fce8043826d407ce2adb05666d49e2bd16b7d9a2
nicojschwarz <nicoschwarz@posteo.de>
2018-01-19 23:02:50 +0100

    i2c test 55

--- index.js
+++ index.js
@@ -2,11 +2,49 @@
 
 const raspi = require('raspi');
 const I2C = require('raspi-i2c').I2C;
- 
+const DigInput = require('raspi-gpio').DigitalInput;
+
+/** @type {I2C} */
+var i2c = null;
+/** @type {DigInput} */
+var btnPin = null;
 raspi.init(() => {
-  const i2c = new I2C();
-  console.log(i2c.writeByteSync(0x18, 65)); // Read one byte from the device at address 18
+    i2c = new I2C();
+    btnPin = new DigInput();
 });
 
-console.log("Hello World!");
 
+/**
+ * @param {"off"|"on"|"up"|"down"|"blue"|"green"|"red"|number} cmd 
+ */
+function i2cWrite(cmd) {
+    if (type(cmd) === "string")
+        cmd = parseCmd(cmd);
+    if (type(cmd) !== "number" || cmd < 1 || cmd > 7)
+        throw "I2C invalid command";
+
+    if (i2c != null)
+        i2c.writeByte(69, cmd);
+}
+
+/**
+ * @param {"off"|"on"|"up"|"down"|"blue"|"green"|"red"} cmd 
+ */
+function parseCmd(cmd) {
+    var result = {
+        off: 1,
+        on: 2,
+        up: 3,
+        down: 4,
+        blue: 5,
+        green: 6,
+        red: 7
+    }[cmd];
+    if (!result)
+        throw "Invalid i2c command";
+    return result;
+}
+
+i2cWrite("on");
+
+//btnPin.read();  
