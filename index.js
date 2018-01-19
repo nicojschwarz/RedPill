d64f71ee48971903a9eeafc8bec045fe26376785
nicojschwarz <nicoschwarz@posteo.de>
2018-01-19 22:02:31 +0100

    i2c test 2

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 "use strict"
 
-var raspi = require('raspi');
-var I2C = require('raspi-i2c').I2C;
-
+const raspi = require('raspi');
+const I2C = require('raspi-i2c').I2C;
+ 
 raspi.init(() => {
-    const i2c = new I2C();
-    console.log(i2c.writeByteSync(0x69, 6));
+  const i2c = new I2C();
+  console.log(i2c.readByteSync(0x18)); // Read one byte from the device at address 18
 });
 
 console.log("Hello World!");
