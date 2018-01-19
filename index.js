1b5ada4d4ea3657ff835037522ae79c879d3074c
nicojschwarz <nicoschwarz@posteo.de>
2018-01-19 23:32:57 +0100

    test

--- index.js
+++ index.js
@@ -1,52 +1,14 @@
+import { setTimeout } from 'timers';
+
 "use strict"
 
-const raspi = require('raspi');
-const I2C = require('raspi-i2c').I2C;
-const DigInput = require('raspi-gpio').DigitalInput;
+const hw = require('./util/hardware')
 
-/** @type {I2C} */
-var i2c = null;
-/** @type {DigInput} */
-var btnPin = null;
-raspi.init(() => {
-    i2c = new I2C();
-    i2cWrite("on");
-    //btnPin = new DigInput();
-});
-
-
-/**
- * @param {"off"|"on"|"up"|"down"|"blue"|"green"|"red"|number} cmd 
- */
-function i2cWrite(cmd) {
-    if (typeof cmd === "string")
-        cmd = parseCmd(cmd);
-    if (typeof cmd !== "number" || cmd < 1 || cmd > 7)
-        throw "I2C invalid command";
-
-    console.log("sending " + cmd);
-    if (i2c != null)
-        i2c.writeByte(69, cmd);
+function test(i) {
+    hw.i2cWrite(i);
+    if (i < 7)
+        setTimeout(test, 2000, i + 1);
 }
-
-/**
- * @param {"off"|"on"|"up"|"down"|"blue"|"green"|"red"} cmd 
- */
-function parseCmd(cmd) {
-    var result = {
-        off: 1,
-        on: 2,
-        up: 3,
-        down: 4,
-        blue: 5,
-        green: 6,
-        red: 7
-    }[cmd];
-    if (!result)
-        throw "Invalid i2c command";
-    return result;
-}
-
-
-
-//btnPin.read();  
+setTimeout(() => {
+    test(1);
+}, 1000);
---
+++ util/hardware.js
@@ -0,0 +1,50 @@
+"use strict"
+
+const raspi = require('raspi');
+const I2C = require('raspi-i2c').I2C;
+const DigInput = require('raspi-gpio').DigitalInput;
+
+/** @type {I2C} */
+var i2c = null;
+/** @type {DigInput} */
+var btnPin = null;
+raspi.init(() => {
+    i2c = new I2C();
+    //btnPin = new DigInput();
+});
+
+
+/**
+ * @param {"off"|"on"|"up"|"down"|"blue"|"green"|"red"|number} cmd 
+ */
+export function i2cWrite(cmd) {
+    if (typeof cmd === "string")
+        cmd = parseCmd(cmd);
+    if (typeof cmd !== "number" || cmd < 1 || cmd > 7)
+        throw "I2C invalid command";
+    
+    if (i2c != null)
+        i2c.writeByte(69, cmd);
+}
+
+/**
+ * @param {"off"|"on"|"up"|"down"|"blue"|"green"|"red"} cmd 
+ */
+function parseCmd(cmd) {
+    var result = {
+        off: 1,
+        on: 2,
+        up: 3,
+        down: 4,
+        blue: 5,
+        green: 6,
+        red: 7
+    }[cmd];
+    if (!result)
+        throw "Invalid i2c command";
+    return result;
+}
+
+
+
+//btnPin.read();  
