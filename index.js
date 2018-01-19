f3399e297f9137e2be2f697486a4c4073376181b
nicojschwarz <nicoschwarz@posteo.de>
2018-01-19 23:55:28 +0100

    express

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 "use strict"
 
-const hw = require('./util/hardware')
+const hw = require('./util/hardware');
+const express = require('express');        // call express
+const bodyParser = require('body-parser');
 
-function test(i) {
-    hw.i2cWrite(i);
-    if (i < 7)
-        setTimeout(test, 2000, i + 1);
-}
-setTimeout(() => {
-    test(1);
-}, 1000);
+const app = express();                 // define our app using express
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const tmpRouter = express.Router();
+
+tmpRouter.get("/:cmd", (req, res, next) => {
+    try {
+        hw.i2cWrite(req.params.cmd);
+        res.send("success");
+    } catch (err) { next(err); }
+});
+
+app.use("/", tmpRouter)
+
+app.listen(80);
