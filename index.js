bc2f3fde3d01396fbec19967fdc7264785573b4e
Nico Schwarz <nicoschwarz@posteo.com>
2018-01-26 20:12:20 +0100

    asdasdsdf

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ const fileUpload = require('express-fileupload');
 
 const app = express();                 // define our app using express
 
+app.use("/sound", require('./router/soundRouter'));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
 app.use("/led", require('./router/ledRouter'));
 app.use("/alarm", require('./router/alarmRouter'));
-app.use("/sound", require('./router/soundRouter'));
 
 app.listen(80);
