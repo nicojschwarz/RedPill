532ee0f6a6fa1cc0d038924e969ff6f171a82402
Nico Schwarz <nicoschwarz@posteo.com>
2018-01-27 23:00:47 +0100

    asdasdasdasdasdasdasd

--- util/sound.js
+++ util/sound.js
@@ -16,7 +16,6 @@ exports = module.exports = {};
 exports.play = play;
 exports.hadError = hadError;
 exports.stop = function () {
-    console.log("stopping sound; proc: " + proc);
     if (sound) {
         sound.stop();
         sound = null;
