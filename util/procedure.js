e7b8ff75fd1e595d5e264d36f87619b4e9e48d14
Nico Schwarz <nicoschwarz@posteo.com>
2018-01-27 15:45:42 +0100

    adasdasd

--- util/procedure.js
+++ util/procedure.js
@@ -12,24 +12,74 @@ var hw = require("./hardware");
  *   - if OnRing(running) ->  OnRing(disable mp3)
  */
 
+function delay(fn, t) {
+    // private instance variables
+    var queue = [],
+        self,
+        timer;
+
+    function schedule(fn, t) {
+        timer = setTimeout(function () {
+            timer = null;
+            fn();
+            if (queue.length) {
+                var item = queue.shift();
+                schedule(item.fn, item.t);
+            }
+        }, t);
+    }
+    self = {
+        delay: function (fn, t) {
+            // if already queuing things or running a timer, 
+            //   then just add to the queue
+            if (queue.length || timer) {
+                queue.push({ fn: fn, t: t });
+            } else {
+                // no queue or timer yet, so schedule the timer
+                schedule(fn, t);
+            }
+            return self;
+        },
+        repeat: function (fn, t, n) {
+            if (!queue.length && !timer) {
+                n--;
+                schedule(fn, t);
+            }
+            for (let i = 0; i < n; i++) {
+                queue.push({ fn: fn, t: t });
+            }
+            return self;
+        },
+        cancel: function () {
+            clearTimeout(timer);
+            queue = [];
+            return self;
+        }
+    };
+    return self.delay(fn, t);
+}
+
+
+var d = delay(() => { console.log("1"); }, 500)
+    .delay(() => { console.log("2"); }, 700)
+    .delay(() => { console.log("3"); }, 600)
+    .repeat(() => { console.log("4"); }, 1000, 5);
+
 class Procedure {
     constructor() {
         this.interval = null;
     }
-    onRingNext(cb, ms) {
-        this.interval = setInterval(cb, ms);
-    }
-
-    repeatNTime(cb, n, ms, cb2) {
-        console.log(n);
-        cb();
-        if (n <= 0)
-            cb2();
-        else
-            this.interval = setInterval(this.repeatNTime, ms, cb, n - 1, ms, cb2);
-    }
 
     onRing() {
+        return delay(() => { hw.i2cWrite("on"); }, 0)
+            .repeat(() => { hw.i2cWrite("down"); }, 750, 7)
+            .delay(() => { hw.i2cWrite(alarm.alarm.colorFade); }, 750)
+            .repeat(() => { hw.i2cWrite("up"); }, 128571, 7)
+            .delay(() => { sound.play(); }, 300000)
+            .delay(() => { hw.i2cWrite("on"); }, 3600000)
+            .repeat(() => { hw.i2cWrite("down"); }, 750, 7)
+            .delay(() => { hw.i2cWrite(alarm.alarm.colorReset); }, 750)
+            .delay(() => { hw.i2cWrite("off"); }, 750);
         hw.i2cWrite(alarm.alarm.colorFade);
         this.onRingNext(() => {
             hw.i2cWrite("on");
@@ -39,11 +89,11 @@ class Procedure {
                 this.onRingNext(() => {
                     this.repeatNTime(() => { hw.i2cWrite("down"); }, 7, 128571, () => {
                         this.onRingNext(() => {
-                            sound.play();
+
                         }, 300000);
                         this.onRingNext(() => {
-                            hw.i2cWrite("on");
-                            this.repeatNTime(() => { hw.i2cWrite("down"); }, 7, 750, () => { hw.i2cWrite(alarm.alarm.colorReset); hw.i2cWrite("off"); });
+
+                            this.repeatNTime(() => { hw.i2cWrite("down"); }, 7, 750, () => { hw.i2cWrite("off"); });
                         }, 3600000);
                     });
                 }, 128571)
